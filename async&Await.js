e5f9ea60775a4bd6058a7e4bdd52a6a945044ea8
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-05-26 12:12:09 -0300

    docs: add notes about async and await

--- async&Await.js
+++ async&Await.js
@@ -20,16 +20,42 @@ function esperaAi(msg, tempo) {
 }
 
 // encadeamento de then (um so acontece apos o outro)
-esperaAi('Fase 1', random())
-    .then(fase1 => { 
-        console.log (fase1)
-        return esperaAi('Fase 2', random())
-    })
-    .then(fase2 => {
+
+// esperaAi('Fase 1', random())
+//     .then(fase1 => { 
+//         console.log (fase1)
+//         return esperaAi('Fase 2', random())
+//     })
+//     .then(fase2 => {
+//         console.log(fase2)
+//         return esperaAi('Fase 3', random())
+//     })
+//     .then(fase3 => {
+//         console.log(fase3)
+//     })
+//     .catch(err => console.log(err))
+
+
+// async - serve para marcar uma função como assíncrona, ou seja, ela pode conter código assíncrono dentro dela. 
+
+async function executa() {
+
+    try { // para englobar e fazer apenas um catch
+        
+        const fase1 = await esperaAi('Fase 1', random()) // await - serve para esperar a resolução de uma promessa. Ele pausa a execução da função assíncrona até que a promessa seja resolvida e retorna o valor resolvido.
+        console.log(fase1)
+        
+        const fase2 = await esperaAi('Fase 2', random())
         console.log(fase2)
-        return esperaAi('Fase 3', random())
-    })
-    .then(fase3 => {
+
+        const fase3 = await esperaAi('Fase 3', random())
         console.log(fase3)
-    })
-    .catch(err => console.log(err))
+
+        console.log('Terminamos na fase: ' + fase3)
+    } catch(err) { // Este catch é global e serve para todos os await, mas posso fazer um catch para cada
+        console.log(err)
+    }
+    
+}
+
+executa() // executa de forma ordenada mais organizada
